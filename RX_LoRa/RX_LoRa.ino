27fdeae1e14b2b01b6b5b9213c9d4a1fb697ec1a
daniela <daniela.lourenco@efacec.pt>
2018-01-31 16:46:36 +0000

    added a validatePacket function to rx mode

--- RX_LoRa/RX_LoRa.ino
+++ RX_LoRa/RX_LoRa.ino
@@ -74,6 +74,53 @@ void setup()
   Serial.println();
 }
 
+void validatePacketsReceive(){
+  
+    uint8_t last_packetNumber;
+    int packetLost;
+    uint8_t voltas;
+    int totalPackets;
+    int packetLoss;
+    totalPackets++;
+    uint8_t e;
+    
+    if(sx1278._packetNumber!=255){
+       e=sx1278._packetNumber-last_packetNumber;
+       
+       if(e>1){
+          packetLost=packetLost+e;
+       }
+       
+    }
+    
+    if(sx1278._packetNumber == 255 || ((last_packetNumber==254 || last_packetNumber==253 || last_packetNumber==252 ) && sx1278._packetNumber==0)){  //needed other verification
+        voltas++;
+        Serial.println("Voltas: ");
+        Serial.print(voltas);
+    }
+    
+    if(voltas == 4){
+      packetLoss=(packetLost/totalPackets);
+      Serial.println("--------------------------------------------------------------------------------------");
+      Serial.println("Total Packets received in this round: ");
+      Serial.print(totalPackets);
+      Serial.println("Packets Lost: ");
+      Serial.print(packetLost);
+      Serial.println("Packet Loss: ");
+      Serial.print(packetLoss);
+      Serial.println("--------------------------------------------------------------------------------------");
+
+      voltas=0;
+      packetLoss=0;
+      totalPackets=0;
+      packetLost=0;
+   
+    }
+
+    last_packetNumber=sx1278._packetNumber;
+  
+}
+
 void loop(void)
 {
   // Receive message for 10 seconds
@@ -83,22 +130,26 @@ void loop(void)
     delay(500);
     digitalWrite(LORA_LED, LOW);
       
-    Serial.println(F("Package received!"));
+    //Serial.println(F("Package received!"));
 
     for (unsigned int i = 0; i < sx1278.packet_received.length; i++) {
       my_packet[i] = (char)sx1278.packet_received.data[i];
     }
     
-    Serial.print(F("Message: Source | Packet Number ! Packet Lenght | Payload "));
+//    Serial.print(F("Message: Source | Packet Number ! Packet Lenght | Payload "));
+    Serial.print(F("Source | Packet Number ! Packet Lenght | Payload -> "));
     Serial.print(sx1278.packet_received.src);
     Serial.print(" | ");
     Serial.print(sx1278.packet_received.packnum);
     Serial.print(" | ");
     Serial.print(sx1278.packet_received.length);
-    Serial.print(" | ");
-    Serial.println(my_packet);
+    Serial.println(" ");
+//    Serial.print(" | ");
+//    Serial.println(my_packet);
     sx1278.getSNR();
     sx1278.getRSSI();
+    
+    validatePacketsReceive();
     
   } 
   else {
--- RX_LoRa/SX1278.h
+++ RX_LoRa/SX1278.h
@@ -1,3 +1,4 @@
+
 /*! \file SX1278.h
     \brief Library for managing Semtech modules
 
